c4c9cd9b16936775da3d813ca2dceab85ae300e9
WitherMite <43625516+WitherMite@users.noreply.github.com>
2024-02-01 08:50:42 -0600

    correct formatting

--- library.js
+++ library.js
@@ -7,12 +7,12 @@ const SUBMIT_BTN = document.querySelector(".submit-btn");
 
 class Book {
   constructor(title, author, pages, haveRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.haveRead = haveRead;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
   }
-  
+
   toggleRead() {
     this.haveRead = !this.haveRead;
   }
